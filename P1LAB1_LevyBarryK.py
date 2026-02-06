7b348affdb5183a32fdce705bda5b29ec2d1f655
blevy23195-del <blevy23195@gmail.com>
2026-02-06 08:56:55 -0500

    Add files via upload

---
+++ P1LAB1_LevyBarryK.py
@@ -0,0 +1,9 @@
+# Barry Levy
+# February 6, 2026
+# P1LAB1
+# This program takes a user's first and last name as input and displays a welcome message.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
